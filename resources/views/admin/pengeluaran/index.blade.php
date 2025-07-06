2c3e99f44c055ad7e62d2eaa2e9bbd0edd39b95a
mael <ismailgilangm@gmail.com>
2025-07-06 19:30:53 +0700

    haha

--- resources/views/admin/pengeluaran/index.blade.php
+++ resources/views/admin/pengeluaran/index.blade.php
@@ -147,7 +147,7 @@
                 <label for="keteranganSelect" class="block text-gray-700 text-sm font-bold mb-2">
                     Keterangan
                 </label>
-                <select id="keteranganSelect" name="keterangan_field_display" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                <select id="keteranganSelect" name="keterangan_field_display" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" required>
                     <option value="">-- Pilih Keterangan --</option>
                     @foreach($data2 as $d)
                     <!-- Nilai option akan berisi 'kode_akun,keterangan' yang akan dipisahkan oleh JavaScript -->
@@ -163,7 +163,7 @@
 
             <div class="mt-4">
                 <x-input-label for="tanggal" value="tanggal" />
-                <x-text-input id="tanggal" name="tanggal" type="date" class="mt-1 block w-full" />
+                <x-text-input id="tanggal" name="tanggal" type="date" class="mt-1 block w-full" required/>
                 <x-input-error class="mt-2" :messages="$errors->get('tanggal')" />
             </div>
 
